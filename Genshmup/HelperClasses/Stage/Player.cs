a27af6e3cc122824aa037dd091f7b4fe08284db9
Noah2605 <noah@bmasoft.de>
2021-10-13 22:12:29 +0200

    Stage 1 player movable

---
+++ Genshmup/HelperClasses/Stage/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Genshmup.HelperClasses
+{
+    public class Player
+    {
+        public Point Position { get => _position; set => _position = value; }
+        private Point _position;
+
+        public Player()
+        {
+            
+        }
+
+        public void Move(Direction dir)
+        {
+            switch (dir)
+            {
+                case Direction.Up:
+                    _position.Y -= 10;
+                    break;
+                case Direction.Down:
+                    _position.Y += 10;
+                    break;
+                case Direction.Left:
+                    _position.X -= 10;
+                    break;
+                case Direction.Right:
+                    _position.X += 10;
+                    break;
+            }
+        }
+    }
+}
